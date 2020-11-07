e3216ad581ee9ab4a2f12b80432c39d5f1feee41
alex <elkikos@gmail.com>
2020-11-07 01:51:20 +0100

    Added Graph Test

---
+++ MailDucky.Test/GraphTest/AuthTest.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailDucky.Test.GraphTest
+{
+    class AuthTest : MailDuckyBaseTest
+    {
+        [Test]
+        public async Task AuthenticateDiego()
+        {
+            var graphService = new GraphConnector.GraphAuthenticationService(config);
+
+            var user = await graphService.GetUser("DiegoS@devbrett.onmicrosoft.com");
+            Assert.AreEqual(user.DisplayName, "Diego Siciliani");
+        }
+    }
+}
